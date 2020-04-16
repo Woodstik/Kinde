2f5b5b6e58cc05d1730e3dcb5e79cc7a54564a6a
Woodstik <woodstik92@gmail.com>
2020-04-16 17:25:24 -0500

    Login Design (#3)

---
+++ app/src/main/java/com/wstik/kinde/presentation/auth/login/LoginActivity.kt
@@ -0,0 +1,47 @@
+package com.wstik.kinde.presentation.auth.login
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.wstik.kinde.R
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.toolbar.view.*
+
+fun Context.startLogin() {
+    startActivity(Intent(this, LoginActivity::class.java))
+}
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        setSupportActionBar(layoutToolbar.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setTitle(R.string.login_title)
+
+        inputEmail.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(s: Editable?){
+
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int){}
+        })
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
