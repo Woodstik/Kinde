6e337ea8b8fc5875fed2db7e00e0aed9e03ed5b8
jmeneses <woodstik92@gmail.com>
2020-04-17 17:10:59 -0500

    Make throwable public

--- app/src/main/java/com/wstik/kinde/data/enums/LoadState.kt
+++ app/src/main/java/com/wstik/kinde/data/enums/LoadState.kt
@@ -1,7 +1,7 @@
 package com.wstik.kinde.data.enums
 
 sealed class LoadState<in T> {
-    data class Error(private val throwable: Throwable) : LoadState<Any?>()
+    data class Error(val throwable: Throwable) : LoadState<Any?>()
     object Loading : LoadState<Any?>()
     data class Data<T>(val data: T) : LoadState<T>()
 }
