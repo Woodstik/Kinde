7b4885e67a03f93a57907f6ee90b6452081d978a
jmeneses <woodstik92@gmail.com>
2020-04-16 23:54:44 -0500

    View Model implementation for sign up

--- app/src/main/java/com/wstik/kinde/presentation/auth/signup/SignUpActivity.kt
+++ app/src/main/java/com/wstik/kinde/presentation/auth/signup/SignUpActivity.kt
@@ -6,10 +6,15 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.wstik.kinde.R
+import com.wstik.kinde.data.enums.LoadState
+import com.wstik.kinde.utils.hideKeyboard
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import kotlinx.android.synthetic.main.toolbar.view.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 fun Context.startSignUp() {
     startActivity(Intent(this, SignUpActivity::class.java))
@@ -17,22 +22,30 @@ fun Context.startSignUp() {
 
 class SignUpActivity : AppCompatActivity() {
 
+    private val viewModel: SignUpViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        viewModel.signUpState.observe(this, Observer { handleSignUpState(it) })
 
         setSupportActionBar(layoutToolbar.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         setTitle(R.string.sign_up_title)
 
-        inputEmail.addTextChangedListener(object : TextWatcher{
-            override fun afterTextChanged(s: Editable?){
+        buttonSignUp.setOnClickListener {
+            viewModel.signUpEmail(inputEmail.text.toString(), inputPassword.text.toString())
+            hideKeyboard(it)
+        }
+        buttonGoogle.setOnClickListener { viewModel.signUpGoogle() }
+        buttonFacebook.setOnClickListener { viewModel.signUpFacebook() }
+    }
 
-            }
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int){}
-        })
-
+    private fun handleSignUpState(sate: LoadState<Unit>?) {
+        when(sate){
+            is LoadState.Data -> Toast.makeText(this, "User Created!", Toast.LENGTH_SHORT).show()
+            is LoadState.Error -> Toast.makeText(this, "Sign Up Error!", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
--- app/src/main/java/com/wstik/kinde/presentation/auth/signup/SignUpViewModel.kt
+++ app/src/main/java/com/wstik/kinde/presentation/auth/signup/SignUpViewModel.kt
@@ -1,11 +1,43 @@
 package com.wstik.kinde.presentation.auth.signup
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.wstik.kinde.data.enums.LoadState
+import com.wstik.kinde.data.requests.SignUpRequest
+import com.wstik.kinde.domain.SignUpUseCase
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.subscribeBy
 
-class SignUpViewModel : ViewModel(){
+class SignUpViewModel(private val signUpUseCase: SignUpUseCase) : ViewModel() {
+
+    val signUpState = MutableLiveData<LoadState<Unit>>()
+    val formState = MutableLiveData<LoadState<Unit>>()
+    private val compositeDisposable = CompositeDisposable()
+
+    fun signUpEmail(email: String, password: String) {
+        signUp(SignUpRequest.Email(email, password))
+    }
+
+    fun signUpGoogle() {
+        signUp(SignUpRequest.Google)
+    }
+
+    fun signUpFacebook() {
+        signUp(SignUpRequest.Facebook)
+    }
 
     override fun onCleared() {
         super.onCleared()
+        compositeDisposable.clear()
     }
 
+    private fun signUp(request: SignUpRequest) {
+        val disposable = signUpUseCase.execute(request)
+            .doOnSubscribe { signUpState.value = LoadState.Loading }
+            .subscribeBy(
+                onNext = { signUpState.value = LoadState.Data(it) },
+                onError = { signUpState.value = LoadState.Error(it) }
+            )
+        compositeDisposable.add(disposable)
+    }
 }
