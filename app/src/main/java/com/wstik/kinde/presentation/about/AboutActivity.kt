dd94bec4b61b49d5b6e687359622916c871e3ccd
jmeneses <woodstik92@gmail.com>
2020-04-22 23:44:23 -0500

    About Screen

--- app/src/main/java/com/wstik/kinde/presentation/about/AboutActivity.kt
+++ app/src/main/java/com/wstik/kinde/presentation/about/AboutActivity.kt
@@ -4,9 +4,14 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.wstik.kinde.BuildConfig
 import com.wstik.kinde.R
+import kotlinx.android.synthetic.main.activity_about.*
+import kotlinx.android.synthetic.main.row_profile_option.view.*
+import kotlinx.android.synthetic.main.toolbar.view.*
 
-fun Context.startAbout(){
+fun Context.startAbout() {
     startActivity(Intent(this, AboutActivity::class.java))
 }
 
@@ -15,5 +20,21 @@ class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        setSupportActionBar(layoutToolbar.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setTitle(R.string.about_title)
+
+        textVersionInfo.text = getString(R.string.app_version_format, BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
